b8aba01d30ad52cf723b69566439e5f99a27bc69
Matan Peretz <matanpe@mta.ac.il>
2020-05-31 19:33:33 +0300

    Finished writing the Garage class

--- Ex03.GarageLogic/Garage.cs
+++ Ex03.GarageLogic/Garage.cs
@@ -8,13 +8,34 @@ namespace Ex03.GarageLogic
 {
     public class Garage
     {
-        Dictionary<string, InformationOfVeichlie> VehiclesInTheGarage;
-
-
-
-        public class InformationOfVeichlie
+        Dictionary<string, InformationOfVehicle> m_VehiclesInTheGarage;
+        
+        
+        public Garage()
         {
+            m_VehiclesInTheGarage = new Dictionary<string, InformationOfVehicle>();
+        }
 
+        public class InformationOfVehicle
+        {
+            enum eCarStateInGarage
+            {
+                InRepair,
+                Repaired,
+                Paid
+            };
+            eCarStateInGarage m_State;
+            string m_NameOfOwner;
+            string m_PhoneNumber;
+            Vehicle m_Vehicle;
+
+            public InformationOfVehicle(string i_OwnerName, string i_PhoneNumber, Vehicle i_Vehicle)
+            {
+                m_NameOfOwner = i_OwnerName;
+                m_PhoneNumber = i_PhoneNumber;
+                m_State = eCarStateInGarage.InRepair;
+                m_Vehicle = i_Vehicle;
+            }
         }
 
     }
